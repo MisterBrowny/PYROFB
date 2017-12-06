8a560b4e6c9bf2a1f82700e0f85bc809e4567142
MisterBrowny <32779782+MisterBrowny@users.noreply.github.com>
2017-12-06 22:52:38 +0100

    Initial commit

---
+++ PYROFB/PIROFB/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 25/11/2017 15:45:19
+ * Author : Cédric
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
